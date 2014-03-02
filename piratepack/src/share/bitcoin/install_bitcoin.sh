d4130c80551b1ffa124215c8f1f910ea9645cd46
Andrew K <piratelnx@gmail.com>
2014-03-02 13:20:28 -0700

    add cwallet menu icon

--- piratepack/src/share/bitcoin/install_bitcoin.sh
+++ piratepack/src/share/bitcoin/install_bitcoin.sh
@@ -6,3 +6,25 @@ maindir="$(pwd)"
 cd
 homedir="$(pwd)"
 localdir="$homedir"/.piratepack/bitcoin
+
+if [ -d "$maindir/share/cwallet_build" ]
+then
+    if [ ! -d .local ]
+    then mkdir .local
+    fi
+    cd .local
+    if [ ! -d share ]
+    then mkdir share
+    fi
+    cd share
+    if [ ! -d icons ]
+    then mkdir icons
+    fi
+    cp "$maindir/share/cwallet_build/icon.png" icons
+    if [ ! -d applications ]
+    then mkdir applications
+    fi
+    cp "$curdir"/cwallet.desktop "$localdir"
+    echo "Exec=$maindir/bin/cwallet-gui --addpath $maindir/bin" >> "$localdir"/cwallet.desktop
+    mv "$localdir/cwallet.desktop" applications
+fi
